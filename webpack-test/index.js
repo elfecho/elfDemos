b9a03344e47182e4569b08a7fa5f58bdd2c8e9c9
elfecho <317479683@qq.com>
2020-12-07 17:48:45 +0800

    add webpack-demo

---
+++ webpack-test/index.js
@@ -0,0 +1,18 @@
+
+// CMD导入方式
+// import Header from './src/header'
+// import Sidebar from './src/sidebar'
+// import Content from './src/content'
+
+// AMD导入方式
+const Header = require('./src/header')
+const Sidebar = require('./src/sidebar')
+const Content = require('./src/content')
+
+new Header()
+new Sidebar()
+new Content()
+
+
+
+
