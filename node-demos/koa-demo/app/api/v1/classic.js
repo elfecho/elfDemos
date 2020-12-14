f09847c76d9cf0cdc1e70db488ccc9a2bda51234
elfeach <317479683@qq.com>
2020-12-14 23:53:39 +0800

    add Exception

--- node-demos/koa-demo/app/api/v1/classic.js
+++ node-demos/koa-demo/app/api/v1/classic.js
@@ -1,15 +1,21 @@
 const Router=require('koa-router')
 const router=new Router()
 
+
 router.post('/v1/:id/classic/latest', async (ctx, next) => {
   const path = ctx.params
   const query = ctx.request.query
   const headers = ctx.request.header
   const body = ctx.request.body
+  if (true) {
+    const error = new global.errs.ParameterException()
+    
+    // error.requestUrl = `${ctx.method} ${ctx.path}`
+    throw error
+  }
   ctx.body = {
     key: 'classic'
   }
-  throw Error('error')
 })
 
 module.exports = router
