7c17b85e52591b06f04340bfedcfa082bcdb7171
elfeach <317479683@qq.com>
2021-03-01 23:44:01 +0800

    add json demo

---
+++ js-algorithm/linkedList/json.js
@@ -0,0 +1,18 @@
+const json = {
+  a: {
+    b: {
+      c: 1
+    }
+  },
+  d: {
+    e: 2
+  }
+}
+
+const path = ['a', 'b', 'c'];
+let p = json;
+path.forEach(k => {
+  p = p[k];
+})
+console.log(1)
+
