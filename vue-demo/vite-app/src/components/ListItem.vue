d2bed4cc972eda7474cf9590ff0234962005d4bf
elfeach <317479683@qq.com>
2021-03-03 00:26:15 +0800

    add listItem

--- vue-demo/vite-app/src/components/ListItem.vue
+++ vue-demo/vite-app/src/components/ListItem.vue
@@ -1,11 +1,24 @@
 <template>
-  <ul>
-    <li>child</li>
-  </ul>
+  <li @click="handleDelete(index)">{{item}}</li>
 </template>
 
 <script setup>
+import { defineProps, defineEmit, useContext } from "vue";
 
+const props = defineProps({
+  item: String,
+  index: Number
+})
+
+const emit = defineEmit(['handleDelete'])
+
+const ctx = useContext()
+
+const handleDelete = (index) => {
+  emit('handleDelete', index)
+}
+
+console.log(ctx)
 </script>
 
 <style>
--- vue-demo/vite-app/src/views/Home/index.vue
+++ vue-demo/vite-app/src/views/Home/index.vue
@@ -3,20 +3,37 @@
   <div class="container">
     <input type="text" v-model="inputValue">
     <button @click="() => handleClick(inputValue)">提交</button>
-    <list-item></list-item>
+    <ul>
+      <list-item 
+        v-for="(item, index) in list" 
+        :key="index" 
+        :item="item" 
+        :index="index"
+        @handleDelete="deletes"
+      ></list-item>
+    </ul>
+    
   </div>
 </template>
 
 <script setup>
   import NavBar from '@/components/NavBar.vue'
-  import ListItem from '@/components/ListItem.vue'
-  import { ref } from 'vue'
+  import ListItem from '@/components/ListItem.vue'  
+
+  import { reactive, ref } from 'vue'
   
 
   const inputValue = ref('')
+  const list = reactive(['hello'])
   const handleClick = (item) => {
-    console.log(item)
+    list.push(String(item))
+    inputValue.value = ''
   }
+  const deletes = (index) => {
+    console.log(index)
+    list.splice(index, 1)
+  }
+  
 
 </script>
 
