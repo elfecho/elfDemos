7016e6d26f020b4a3b9412b4a0a3c5fbc6b2c569
elfecho <liujunhao@kuke.com>
2022-12-12 18:30:18 +0800

    add qiankun

---
+++ vue-demo/qiandun-demo/b-child/src/views/Home.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Author: Hzh
+ * @Date: 2021-04-23 10:13:38
+ * @LastEditTime: 2021-04-23 15:47:58
+ * @LastEditors: Hzh
+ * @Description:
+-->
+<template>
+  <div class="home">
+    我是子应用B
+    <router-link to="/about">去关于页面</router-link>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
